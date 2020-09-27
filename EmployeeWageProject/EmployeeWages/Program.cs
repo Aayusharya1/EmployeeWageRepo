fc2b3af8687fdbde0db144efb9e0cdf5c3d6178b
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 03:20:38 +0530

    [Aayush] added code for usercase2&3

--- EmployeeWageProject/EmployeeWages/Program.cs
+++ EmployeeWageProject/EmployeeWages/Program.cs
@@ -6,10 +6,20 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
-            if (empCheck == 1) Console.WriteLine("employee is present");
-            else Console.WriteLine("employee is absent");
+            if (empCheck == 1) 
+            {
+                empHrs = 8;
+            }
+            else empHrs =0;
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: "+ empWage);
         }
     }
 }
