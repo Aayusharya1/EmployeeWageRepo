0b5b2dd2d490a1bb6c137089cd9de050c7cdd740
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 03:00:39 +0530

    [Aayush] added UC1 code

---
+++ EmployeeWageProject/EmployeeWages/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmployeeWages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1) Console.WriteLine("employee is present");
+            else Console.WriteLine("employee is absent");
+        }
+    }
+}
