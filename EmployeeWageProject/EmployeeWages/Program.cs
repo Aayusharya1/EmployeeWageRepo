8bb2d9c8134fe51f830a196718e5d6e9a93d82e6
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 03:34:13 +0530

    [Aayush] added code for UC4

--- EmployeeWageProject/EmployeeWages/Program.cs
+++ EmployeeWageProject/EmployeeWages/Program.cs
@@ -6,10 +6,27 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == 1) Console.WriteLine("employee is present");
-            else Console.WriteLine("employee is absent");
+            int empCheck = random.Next(0, 3);
+            switch(empCheck)
+            {
+                case 0:
+                    empHrs = 0;
+                    break;
+                case 1:
+                    empHrs = 4;
+                    break;
+                case 2:
+                    empHrs = 8;
+                    break;
+            }
+
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
