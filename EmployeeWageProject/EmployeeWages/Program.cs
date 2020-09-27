2c61173e7bd6d21158b568568ec2f7b63d538404
aayusharya1 <aayusharya07@gmail.com>
2020-09-28 03:56:44 +0530

    [Aayush] added UC6

--- EmployeeWageProject/EmployeeWages/Program.cs
+++ EmployeeWageProject/EmployeeWages/Program.cs
@@ -6,10 +6,40 @@ namespace EmployeeWages
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int empCheck = random.Next(0, 2);
-            if (empCheck == 1) Console.WriteLine("employee is present");
-            else Console.WriteLine("employee is absent");
+            int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            int totHrs = 0;
+            int totDays = 0;
+
+            for (int i = 0; i < 30; i++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 0:
+                        empHrs = 0;
+                         
+                        break;
+                    case 1:
+                        empHrs = 4;
+                        totHrs += empHrs;
+                        totDays += 1;
+                        break;
+                    case 2:
+                        empHrs = 8;
+                        totHrs += empHrs;
+                        totDays += 1;
+                        break;
+                }
+                if (totHrs <= 100 && totDays <= 20)
+                    empWage += empHrs * EMP_RATE_PER_HOUR;
+                else break;
+            }
+
+            Console.WriteLine("Emp Wage: " + empWage);
         }
     }
 }
